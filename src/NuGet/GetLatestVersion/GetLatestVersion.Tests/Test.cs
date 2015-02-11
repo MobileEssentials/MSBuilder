f04d7a800125d6ade4953f3311636cd105e411b2
Daniel Cazzulino <daniel@cazzulino.com>
2015-02-11 01:16:55 -0300

    Added NuGet.GetLatestVersion block to retrieve package information from nuget.org

---
+++ src/NuGet/GetLatestVersion/GetLatestVersion.Tests/Test.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Build.Construction;
+using Microsoft.Build.Framework;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MSBuilder.NuGet
+{
+    public class Test
+    {
+        const string xmlns = "{http://schemas.microsoft.com/developer/msbuild/2003}";
+        static readonly string MSBuildPath = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSBuild\ToolsVersions\12.0", "MSBuildToolsPath", @"C:\Program Files (x86)\MSBuild\12.0\bin\");
+
+        [Fact]
+		public void when_retrieving_latest_version_then_succeeds()
+		{
+			var task = new GetLatestVersion
+			{
+                BuildEngine = new MockBuildEngine(true),
+                IncludePreRelease = true,
+				PackageId = "xunit"
+			};
+
+			Assert.True(task.Execute());
+
+			Assert.NotEqual("0.0", task.PackageVersion);
+		}
+
+        [Fact]
+        public void when_executing_task_then_succeeds()
+        {
+            var tasksFile = new FileInfo(@"..\..\..\GetLatestVersion\bin\MSBuilder.NuGet.GetLatestVersion.targets").FullName;
+
+            var xmlProject = ProjectRootElement.Create();
+            xmlProject.DefaultTargets = "Build";
+            xmlProject.AddImport(tasksFile);
+            xmlProject.AddImport(Path.Combine(MSBuildPath, "Microsoft.Common.tasks"));
+
+            var buildXml = xmlProject.AddTarget("Build");
+            var taskXml = buildXml.AddTask("GetLatestVersion");
+
+            taskXml.SetParameter("PackageId", "xunit");
+            taskXml.SetParameter("IncludePreRelease", "true");
+            taskXml.AddOutputProperty("PackageVersion", "PackageVersion");
+
+            var msgXml = buildXml.AddTask("Message");
+            msgXml.SetParameter("Text", "PackageVersion=$(PackageVersion)");
+            msgXml.SetParameter("Importance", "high");
+
+            var tempFile = Path.GetTempFileName();
+            xmlProject.Save(tempFile);
+
+            var psi = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                FileName = Path.Combine(MSBuildPath, "MSBuild.exe"),
+                Arguments = tempFile
+            };
+
+            var proc = Process.Start(psi);
+            var output = proc.StandardOutput.ReadToEnd().Trim();
+            var errors = proc.StandardError.ReadToEnd().Trim();
+            if (errors.Length > 0)
+                Assert.True(false, errors);
+
+            proc.WaitForExit();
+
+            Assert.True(proc.ExitCode == 0, output);
+            Console.WriteLine(output);
+        }
+
+    }
+}
---
+++ src/NuGet/GetLatestVersion/GetLatestVersion/Readme.txt
@@ -0,0 +1,34 @@
+﻿MSBuilder: NuGet Package GetLatestVersion
+=========================================
+
+This NuGet package provides the GetLatestVersion inline task, which allows 
+easy retrieval of the latest published version of a NuGet package on 
+nuget.org given its package Id, as well as easily accessing all its SemVer
+components.
+
+Usage:
+
+<Target Name="AfterBuild">
+    <GetLatestVersion PackageId="Moq" IncludePreRelease="true">
+        <!-- All Outputs are "optional" so you just use what you need -->
+
+        <!-- Full version as published in nuget.org -->
+        <Output TaskParameter="PackaverVersion" PropertyName="FullVersion" />
+        <!-- Simplified MAJOR.MINOR.PATCH version, .NET compatible -->
+        <Output TaskParameter="SimpleVersion" PropertyName="Version" />
+
+        <!-- Access individual version components, as defined in SemVer.org -->
+        <Output TaskParameter="Major" PropertyName="Major" />
+        <Output TaskParameter="Minor" PropertyName="Minor" />
+        <Output TaskParameter="Patch" PropertyName="Patch" />
+
+        <!-- Release is optional, typically something like 'alpha', 'pre', etc. -->
+        <Output TaskParameter="Release" PropertyName="Release" />
+
+        <!-- Build metadata is optional, and typically not used since it's 
+             part of SemVer v2 which isn't widely implemented yet. See SemVer.org
+             for more info. -->
+        <Output TaskParameter="Build" PropertyName="Build" />
+
+    </GetLatestVersion>
+</Target>
