caf95bced633b7aacb6f72c2e20ea53028f6db95
Daniel Cazzulino <daniel@cazzulino.com>
2016-08-09 08:17:33 -0300

    Added ThisAssembly.Vsix nuget package to expose VSIX manifest information as constants

---
+++ src/ThisAssembly.Vsix/build/MSBuilder.ThisAssembly.Vsix.vb.pp
@@ -0,0 +1,32 @@
+#Const $NamespaceDefine$ = 1
+#Const $MetadataDefine$ = 1
+
+#If ADDMETADATA
+<Assembly: System.Reflection.AssemblyMetadata("Vsix.Identifier", Global.RootNamespace.ThisAssembly.Vsix.Identifier)>
+<Assembly: System.Reflection.AssemblyMetadata("Vsix.Name", Global.RootNamespace.ThisAssembly.Vsix.Name)>
+<Assembly: System.Reflection.AssemblyMetadata("Vsix.Description", Global.RootNamespace.ThisAssembly.Vsix.DEscription)>
+<Assembly: System.Reflection.AssemblyMetadata("Vsix.Author", Global.RootNamespace.ThisAssembly.Vsix.Author)>
+#End If
+
+#If LOCALNAMESPACE
+Namespace Global._RootNamespace_
+#Else
+Namespace Global
+#End If
+    Partial Class ThisAssembly
+        ''' <summary>Provides access to the current VSIX extension information.</summary>
+        Partial Public Class Vsix
+            ''' <summary>Identifier: $VsixID$</summary>
+            Public Const Identifier = "$VsixID$"
+
+            ''' <summary>Name: $VsixName$</summary>
+            Public Const Name = "$VsixName$"
+
+            ''' <summary>Description: $VsixDescription$</summary>
+            Public Const Description = "$VsixDescription$"
+
+            ''' <summary>Author: $VsixAuthor$</summary>
+            Public Const Author = "$VsixAuthor$"
+        End Class
+    End Class
+End Namespace
